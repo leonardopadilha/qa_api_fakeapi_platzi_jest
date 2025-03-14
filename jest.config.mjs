6370e331a974a0105251884cffcb2ecf45bd3fc6
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-03-14 19:24:57 -0300

    Inclusão de relatório - Jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,7 +8,23 @@ const config = {
   verbose: true,
   clearMocks: true,
   coverageProvider: "v8",
-  collectCoverage: true
+  collectCoverage: true,
+  "reporters": [
+    "default",
+    [
+      "jest-stare",
+      {
+        "resultDir": "results/jest-stare",
+        "reportTitle": "jest-stare!",
+        "additionalResultsProcessors": [
+          "jest-junit"
+        ],
+        "coverageLink": "../../coverage/lcov-report/index.html",
+        "jestStareConfigJson": "jest-stare.json",
+        "jestGlobalConfigJson": "globalStuff.json"
+      }
+    ]
+  ]
 };
 
 export default config;
