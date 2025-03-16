064f9c7d649832604ab84c90c25f024d79a37593
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-03-16 08:34:43 -0300

    Retornando apenas um produto

--- test/products/products.test.js
+++ test/products/products.test.js
@@ -6,6 +6,8 @@ import 'dotenv/config'
 
 const BASE_URL = process.env.BASE_URL
 
+let productId = 0
+
 describe("#Products", () => {
   describe("GET /products", () => {
     it("Deve retornar todos os produtos", async() => {
@@ -14,6 +16,8 @@ describe("#Products", () => {
       expect(response.body.length).toBeGreaterThan(0)
       expect(response.body[getId(response)].id).not.toBeNull()
       expect(response.body[getId(response)].id).not.toBeUndefined()
+
+      productId = response.body[getId(response)].id
     })
 
     it("Não deve retornar um produto inexistente", async() => {
@@ -21,6 +25,37 @@ describe("#Products", () => {
       expect(response.status).toEqual(400)
       expect(response.body.name).toEqual("EntityNotFoundError")
       expect(response.body.message).toContain("Could not find any entity of type")
+    })
+
+    it('Deve retornar um produto válido', async() => {
+      const response = await request(BASE_URL).get(`/products/${productId}`)
+
+      const expectedProductBody = {
+        "id": "",
+        "title": "",
+        "slug": "",
+        "price": "",
+        "description": "",
+        "category": {
+            "id": "",
+            "name": "",
+            "slug": "",
+            "image": "",
+            "creationAt": "",
+            "updatedAt": ""
+        },
+        "images": [
+            ""
+        ],
+        "creationAt": "",
+        "updatedAt": ""
+    }
+
+    console.log("tipo: " + typeof expectedProductBody.id)
+
+      expect(response.status).toEqual(200)
+      expect(Object.keys(response.body)).toEqual(Object.keys(expectedProductBody))
+
     })
   })
 })
